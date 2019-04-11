baa8fc8a4bcfc4269331f166bb799d4daf9cfdbd
Almog Hamdani <almog@almogs-imac.home>
2019-04-11 22:48:11 +0300

    Update debug script to remove old builds

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -3,6 +3,9 @@
 # Get the script's dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Clean the old build in case it's not built with debug symbols
+$DIR/clean.sh
+
 # Build the iso
 $DIR/create-bootstrap-iso.sh DEBUG
 
