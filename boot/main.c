2623870ff382901087f0f99c6eb6058912a69ccb
Almog Hamdani <almog@almogs-imac.home>
2019-04-13 00:50:21 +0300

    Add multiboot verification

--- boot/main.c
+++ boot/main.c
@@ -10,6 +10,22 @@ void boot_main(unsigned long magic, unsigned long addr)
 
     // Print Bootstrap info
     printf("%s Bootstrap, v%s\n", OS_NAME, BOOTSTRAP_VERSION);
+
+    // Check multiboot2 magic number
+    printf("Multiboot2 Magic Number: 0x%x\n", magic);
+    if (magic != MULTIBOOT2_BOOTLOADER_MAGIC)
+    {
+        printf("Invalid Multiboot2 magic number!\n");
+        return;
+    }
+
+    // Check if the MBI is aligned
+    printf("Multiboot2 Info Structure Address: 0x%x\n", addr);
+    if (addr & 7)
+    {
+        printf("MBI not aligned!\n");
+        return;
+    }
     
     asm("hlt");
 }
