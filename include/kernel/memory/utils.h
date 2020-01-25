72acc36b5ccd1e237a2ab86762f5433f97cc13b1
Almog Hamdani <almog@Almogs-iMac.local>
2020-01-25 04:27:40 +0200

    feat: implement memset and memcpy

--- include/kernel/memory/utils.h
+++ include/kernel/memory/utils.h
@@ -13,8 +13,11 @@ class utils {
     static uint64_t patch_page_address(uint64_t address);
     static uint64_t get_page_entry_index(uint64_t address);
 
-    static uint64_t count_physical_memory(boot_info_mem &mem_info);
+    static uint64_t count_physical_memory(const boot_info_mem &mem_info);
     static uint64_t calc_amount_of_pages_for_bitmap(uint64_t bitmap_size);
+
+    static void memset(void *ptr, uint8_t value, uint64_t amount);
+    static void memcpy(void *dst, void *src, uint64_t amount);
 };
 };  // namespace memory
 };  // namespace influx
--- kernel/memory/utils.cpp
+++ kernel/memory/utils.cpp
@@ -1,5 +1,4 @@
 #include <kernel/memory/utils.h>
-
 #include <memory/paging.h>
 
 void *influx::memory::utils::get_pml4() {
@@ -11,8 +10,7 @@ void *influx::memory::utils::get_pml4() {
     return (void *)cr3;
 }
 
-uint64_t influx::memory::utils::patch_page_address_set_value(uint64_t address)
-{
+uint64_t influx::memory::utils::patch_page_address_set_value(uint64_t address) {
     return address >> 12;
 }
 
@@ -22,7 +20,7 @@ uint64_t influx::memory::utils::patch_page_address(uint64_t address) {
 
 uint64_t influx::memory::utils::get_page_entry_index(uint64_t address) { return address & 0x1FF; }
 
-uint64_t influx::memory::utils::count_physical_memory(boot_info_mem &mem_info) {
+uint64_t influx::memory::utils::count_physical_memory(const boot_info_mem &mem_info) {
     uint64_t mem_size = 0;
 
     // For each entry add it's size to the mem size
@@ -35,4 +33,25 @@ uint64_t influx::memory::utils::count_physical_memory(boot_info_mem &mem_info) {
 
 uint64_t influx::memory::utils::calc_amount_of_pages_for_bitmap(uint64_t bitmap_size) {
     return (bitmap_size / PAGE_SIZE) + (bitmap_size % PAGE_SIZE ? 1 : 0);
+}
+
+void influx::memory::utils::memset(void *ptr, uint8_t value, uint64_t amount) {
+    for (uint64_t i = 0; i < amount; i++) {
+        *((uint8_t *)ptr + i) = value;
+    }
+}
+
+void influx::memory::utils::memcpy(void *dst, void *src, uint64_t amount) {
+    // Copy chunks of 8 byte from src to dst
+    for (uint64_t i = 0; i < amount / sizeof(uint64_t); i++) {
+        *((uint64_t *)dst + i) = *((uint64_t *)src + i);
+    }
+
+    // Check if there is more bytes remaining to copy
+    if (amount % sizeof(uint64_t)) {
+        for (uint8_t i = 0; i < amount % sizeof(uint64_t); i++) {
+            *((uint8_t *)dst + amount - ((amount % sizeof(uint64_t)) - i)) =
+                *((uint8_t *)src + amount - ((amount % sizeof(uint64_t)) - i));
+        }
+    }
 }
