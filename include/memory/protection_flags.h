9e2aef2e786bc52fdf495fcfffc687702c6899ba
Almog Hamdani <almogh52@gmail.com>
2020-01-27 12:38:31 +0200

    feat: create a custom type for the protection flags

--- include/memory/protection_flags.h
+++ include/memory/protection_flags.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <stdint.h>
+
+typedef uint8_t protection_flags_t;
+
 #define PROT_NONE 0
 #define PROT_READ 1
 #define PROT_WRITE 2
--- include/memory/vma_region.h
+++ include/memory/vma_region.h
@@ -2,10 +2,11 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <memory/protection_flags.h>
 
 typedef struct vma_region {
 	uint64_t base_addr;
 	uint64_t size;
-	uint8_t protection_flags;
+	protection_flags_t protection_flags;
 	bool allocated;
 } vma_region_t;
