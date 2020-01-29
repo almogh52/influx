40f264234f8b9eeaebbf88164116bd534c99d15a
Almog Hamdani <almog@Almogs-iMac.local>
2020-01-29 17:54:17 +0200

    feat: add missing delete functions

--- kernel/memory/free.cpp
+++ kernel/memory/free.cpp
@@ -5,8 +5,18 @@ void operator delete(void *p)
 {
     kfree(p);
 }
+
+void operator delete(void*p, long unsigned int)
+{
+    kfree(p);
+}
  
 void operator delete[](void *p)
+{
+    kfree(p);
+}
+
+void operator delete[](void*p, long unsigned int)
 {
     kfree(p);
 }
