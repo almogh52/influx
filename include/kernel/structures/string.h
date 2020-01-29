75a6befb3dcbbb4522461eff14da51d0aa6b5219
Almog Hamdani <almog@Almogs-iMac.local>
2020-01-29 18:04:39 +0200

    feat: implement string structure (#17)

---
+++ include/kernel/structures/string.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+namespace influx {
+namespace structures {
+class string {
+   public:
+    typedef char value_type;
+    typedef value_type* pointer;
+    typedef const value_type* const_pointer;
+    typedef value_type& reference;
+    typedef value_type const_reference;
+    static constexpr const size_t npos = INT64_MAX;  ///< Value that means the end of string.
+    static constexpr const size_t capacity_alignment = 32;
+
+    string(const_pointer s, size_t len);
+    string(size_t n, value_type c);
+    ~string();
+    inline string() : string("", 0) {}
+    inline string(const_pointer s) : string(s, string::length(s)) {}
+    inline string(const string& s) : string(s, s.length()) {}
+
+    inline pointer data() { return _data; }
+    inline const_pointer data() const { return _data; }
+    inline const_pointer c_str() const { return _data; }
+
+    void resize(size_t n, value_type c);
+    inline size_t capacity() const { return _capacity; }
+    inline void resize(size_t n) { resize(n, 0); }
+    inline void clear() { resize(0); }
+
+    reference at(size_t pos);
+    inline const_reference at(size_t pos) const { return at(pos); }
+    inline const_reference front() const { return at(0); }
+    inline reference front() { return at(0); }
+    inline const_reference back() const { return at(length() - 1); }
+    inline reference back() { return at(length() - 1); }
+
+    static size_t length(const_pointer s);
+    inline size_t length() const { return string::length(*this); }
+    inline size_t size() const { return length(); }
+
+    string& append(const_pointer s, size_t len);
+    string& append(size_t n, value_type c);
+    inline string& append(const_pointer s) { return append(s, string::length(s)); }
+    inline string& append(const string& s) { return append(s, s.length()); }
+
+    string& assign(const_pointer s, size_t len);
+    string& assign(size_t n, value_type c);
+    inline string& assign(const_pointer s) { return assign(s, string::length(s)); }
+    inline string& assign(const string& s) { return assign(s, s.length()); }
+
+	static int compare(const_pointer s1, const_pointer s2);
+    inline int compare(const_pointer s) const { return string::compare(*this, s); }
+    inline int compare(const string& s) const { return compare((const_pointer)s); }
+
+    inline operator const_pointer() const { return _data; }
+    inline operator pointer() const { return _data; }
+
+    string operator+(const string& s) const;
+    inline const string& operator=(const string& s) { return assign(s); }
+    inline const string& operator=(const_pointer s) { return assign(s); }
+    inline const string& operator=(const_reference c) { return assign(1, c); }
+    inline const string& operator+=(const string& s) { return append(s); }
+    inline const string& operator+=(const_pointer s) { return append(s); }
+    inline const string& operator+=(value_type c) { return append(1, c); }
+    inline bool operator==(const string& s) const { return compare(s) == 0; }
+    inline bool operator==(const_pointer s) const { return compare(s) == 0; }
+    inline bool operator==(value_type c) const { return length() == 1 && c == at(0); }
+    inline bool operator!=(const string& s) const { return !operator==(s); }
+    inline bool operator!=(const_pointer s) const { return !operator==(s); }
+    inline bool operator!=(value_type c) const { return !operator==(c); }
+
+   private:
+    size_t _capacity;
+    pointer _data;
+
+    inline static constexpr size_t capacity_for_string_size(size_t size) {
+        return (size + 1) % capacity_alignment == 0
+                   ? size + 1
+                   : size + 1 + (capacity_alignment - ((size + 1) % capacity_alignment));
+    }
+};
+};  // namespace structures
+};  // namespace influx
---
+++ kernel/structures/string.cpp
@@ -0,0 +1,145 @@
+#include <kernel/assert.h>
+#include <kernel/memory/heap.h>
+#include <kernel/memory/utils.h>
+#include <kernel/structures/string.h>
+
+influx::structures::string::string(influx::structures::string::const_pointer s, size_t len)
+    : _capacity(string::capacity_for_string_size(len)), _data((pointer)kcalloc(0, _capacity)) {
+    kassert(_data != nullptr);
+    memory::utils::memcpy(_data, s, len);
+}
+
+influx::structures::string::string(size_t n, influx::structures::string::value_type c)
+    : _capacity(string::capacity_for_string_size(n)), _data((pointer)kcalloc(0, _capacity)) {
+    kassert(_data != nullptr);
+    memory::utils::memset(_data, c, n);
+}
+
+influx::structures::string::~string() {
+    // If the data isn't null
+    if (_capacity > 0 && _data) {
+        kfree(_data);
+    }
+}
+
+void influx::structures::string::resize(size_t n, influx::structures::string::value_type c) {
+    pointer new_data = nullptr;
+
+    size_t str_len = length();
+
+    // If the current capacity isn't sufficient for the new string size
+    if (n + 1 > _capacity) {
+        // Allocate the new data
+        _capacity = string::capacity_for_string_size(n);
+        new_data = (pointer)kcalloc(0, _capacity);
+
+        // Copy old data
+        memory::utils::memcpy(new_data, _data, str_len);
+
+        // Free the old data
+        if (_data) {
+            kfree(_data);
+        }
+
+        // Set the new data as the data
+        _data = new_data;
+    }
+
+    // If the new string length is bigger than the current length
+    if (n > str_len) {
+        // Extend the string and set it to the wanted character
+        for (size_t i = str_len; i < n; i++) {
+            _data[i] = c;
+        }
+    }
+
+    // Set the end of the string
+    _data[n] = 0;
+}
+
+influx::structures::string::reference influx::structures::string::at(size_t pos) {
+    // Check for valid size
+    kassert(pos < length());
+
+    return _data[pos];
+}
+
+size_t influx::structures::string::length(const_pointer s) {
+    size_t len = 0;
+
+    // While we didn't reach the end of the string
+    while (*(s + len)) {
+        len++;
+    }
+
+    return len;
+}
+
+influx::structures::string &influx::structures::string::append(
+    influx::structures::string::const_pointer s, size_t len) {
+    size_t str_len = length();
+
+    // Resize the string
+    resize(str_len + len);
+
+    // Copy the data from the other string
+    memory::utils::memcpy(_data + str_len, s, len);
+
+    return *this;
+}
+
+influx::structures::string &influx::structures::string::append(
+    size_t n, influx::structures::string::value_type c) {
+    size_t str_len = length();
+
+    // Resize the string
+    resize(str_len + n);
+
+    // Set the character n times in the data
+    memory::utils::memset(_data + str_len, c, n);
+
+    return *this;
+}
+
+influx::structures::string &influx::structures::string::assign(
+    influx::structures::string::const_pointer s, size_t len) {
+    size_t new_capacity = string::capacity_for_string_size(len);
+
+    // If the new capacity differs from the current capacity
+    if (new_capacity != _capacity) {
+        // Free the old data
+        if (_data) {
+            kfree(_data);
+        }
+
+        // Allocate the new data
+        _capacity = new_capacity;
+        _data = (pointer)kcalloc(0, new_capacity);
+    }
+
+    // Copy the data from the string to the data
+    memory::utils::memcpy(_data, s, len + 1);
+
+    return *this;
+}
+
+int influx::structures::string::compare(influx::structures::string::const_pointer s1,
+                                        influx::structures::string::const_pointer s2) {
+    // While the 2 strings didn't finish and the characters are matching
+    while (*s1 && *s2 && *s1 == *s2) {
+        ++s1;
+        ++s2;
+    }
+
+    return (unsigned char)(*s1) - (unsigned char)(*s2);
+}
+
+influx::structures::string influx::structures::string::operator+(
+    const influx::structures::string &s) const {
+    string new_str(*this);
+
+    // Add the other string to the new string
+    new_str += s;
+
+    return new_str;
+}
