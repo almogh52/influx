b40681cc9d00e0aab16a45b41fbc60140e6afe63
Almog Hamdani <almogh52@gmail.com>
2020-02-06 18:23:32 +0200

    feat: print kernel name and version

---
+++ include/kernel/kernel.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define OS_NAME "Influx"
+#define KERNEL_VERSION "0.2.0"
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,3 +1,4 @@
+#include <kernel/kernel.h>
 #include <kernel/console/console.h>
 #include <kernel/console/early_console.h>
 #include <kernel/memory/physical_allocator.h>
@@ -10,6 +11,8 @@ extern "C" void _start(const boot_info info) {
 
     // Init early console
     influx::console::set_console(new influx::early_console());
+    influx::console::print("%s Kernel, v%s\n", OS_NAME, KERNEL_VERSION);
+    influx::console::print("Made by Almog Hamdani.\n\n\n");
     influx::console::print("Finished initializing memory manager and console.\n");
 
     asm("hlt");
