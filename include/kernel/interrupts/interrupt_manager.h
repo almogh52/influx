435d73e34db1b86395e9d4f783c8c0dbb89c7fc0
Almog Hamdani <almogh52@gmail.com>
2020-02-15 01:09:09 +0200

    feat: implement load ISR function in interrupt manager (#22)

--- include/kernel/interrupts/interrupt_manager.h
+++ include/kernel/interrupts/interrupt_manager.h
@@ -1,6 +1,9 @@
 #pragma once
-#include <kernel/logger.h>
+#include <stdint.h>
+
 #include <interrupt_descriptor.h>
+#include <kernel/interrupts/interrupt_service_routine.h>
+#include <kernel/logger.h>
 
 #define AMOUNT_OF_INTERRUPT_DESCRIPTORS 256
 #define IDT_SIZE (sizeof(interrupt_descriptor_t) * AMOUNT_OF_INTERRUPT_DESCRIPTORS)
@@ -11,12 +14,13 @@ class interrupt_manager {
    public:
     interrupt_manager();
 
+    void set_interrupt_service_routine(uint8_t interrupt_index, interrupt_service_routine isr);
+
    private:
     logger _log;
     interrupt_descriptor_t *_idt;
 
     void load_idt();
-
 };
 };  // namespace interrupts
 };  // namespace influx
--- kernel/interrupts/interrupt_manager.cpp
+++ kernel/interrupts/interrupt_manager.cpp
@@ -1,19 +1,44 @@
 #include <descriptor_table_register.h>
+
+#include <kernel/gdt.h>
 #include <kernel/interrupts/interrupt_manager.h>
+#include <kernel/memory/utils.h>
 #include <kernel/memory/virtual_allocator.h>
 
 influx::interrupts::interrupt_manager::interrupt_manager()
     : _log("Interrupt Manager"),
       _idt((interrupt_descriptor_t *)memory::virtual_allocator::allocate(IDT_SIZE,
                                                                          PROT_READ | PROT_WRITE)) {
-    _log("IDT allocated in %p.\n", _idt);
+    // Init the IDT
+    memory::utils::memset(_idt, 0, IDT_SIZE);
+    _log("IDT allocated and initialized in the address %p.\n", _idt);
 
-	// Load the IDT
-	load_idt();
+    // Load the IDT
+    load_idt();
+}
+
+void influx::interrupts::interrupt_manager::set_interrupt_service_routine(
+    uint8_t interrupt_index, influx::interrupts::interrupt_service_routine isr) {
+    interrupt_descriptor_t descriptor{.offset_1 = (uint16_t)(isr.routine_address & 0xFFFF),
+                                      .selector = (uint8_t)gdt_selector_types::code_descriptor,
+                                      .interrupt_stack_table_index = 0,
+                                      .attributes = 0,
+                                      .offset_2 = (uint16_t)((isr.routine_address >> 16) & 0xFFFF),
+                                      .offset_3 = (uint32_t)(isr.routine_address >> 32),
+                                      .zero_2 = 0};
+
+    // Set the attributes of the descriptor
+    descriptor.type = (uint16_t)isr.type & 0xF;
+    descriptor.present = true;
+
+    // Set the descriptor in the IDT for the ISR
+    _idt[interrupt_index] = descriptor;
+    _log("ISR (address = %p, type = %d) has been set for interrupt %x.", isr.routine_address,
+         isr.type, interrupt_index);
 }
 
 void influx::interrupts::interrupt_manager::load_idt() {
-    descriptor_table_register_t idtr {.limit = IDT_SIZE, .base_virtual_address = (uint64_t)_idt};
+    descriptor_table_register_t idtr{.limit = IDT_SIZE, .base_virtual_address = (uint64_t)_idt};
 
     // Load IDT
     __asm__ __volatile__(
@@ -22,6 +47,6 @@ void influx::interrupts::interrupt_manager::load_idt() {
         :
         : "m"(idtr)
         : "rax");
-		
+
     _log("IDT loaded.\n");
 }
