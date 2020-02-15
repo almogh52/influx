23937f47297cd4132e32cd6650ace2af6cf8911f
Almog Hamdani <almogh52@gmail.com>
2020-02-15 02:24:34 +0200

    feat: add struct for interrupt frame (#22)

--- include/kernel/interrupts/interrupt_service_routine.h
+++ include/kernel/interrupts/interrupt_service_routine.h
@@ -3,9 +3,17 @@
 
 namespace influx {
 namespace interrupts {
-enum interrupt_service_routine_type {
+enum class interrupt_service_routine_type {
     interrupt_gate = INTERRUPT_GATE_TYPE,
     trap_gate = TRAP_GATE_TYPE
+};
+
+struct interrupt_frame {
+    uint64_t rip;
+    uint64_t rflags;
+    uint64_t cs;
+    uint64_t rsp;
+    uint64_t ss;
 };
 
 struct interrupt_service_routine {
