459ed8a24bc7bdc9cd2e617341ef7f88e6156468
Almog Hamdani <almogh52@gmail.com>
2020-02-25 18:31:12 +0200

    fix: fix exception interrupt wrong print parameters

--- kernel/interrupts/exception_interrupt_handler.cpp
+++ kernel/interrupts/exception_interrupt_handler.cpp
@@ -6,7 +6,7 @@
 __attribute__((interrupt)) void influx::interrupts::exception_interrupt_handler(
     influx::interrupts::interrupt_frame *frame, uint64_t error_code) {
     logger log("Exception Interrupt");
-    log("CPU Exception occurred with code %x in address %p.\n", frame->rip, error_code);
+    log("CPU Exception occurred with code %x in address %p.\n", error_code, frame->rip);
 
 	// Panic
 	kpanic();
