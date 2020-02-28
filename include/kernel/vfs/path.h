f0fd83aec81a8b448fbceb77e07fe9694b169278
Almog Hamdani <almogh52@gmail.com>
2020-02-29 00:49:05 +0200

    refactor: rewrite path implementation and support relative paths

--- include/kernel/vfs/path.h
+++ include/kernel/vfs/path.h
@@ -7,22 +7,29 @@ namespace vfs {
 class path {
    public:
     path() = default;
-    path(structures::string& path_str);
+    path(const structures::string& path_str);
 
     path& operator=(structures::string path);
 
-    inline structures::string string() const;
+    bool is_parent(const path& p) const;
+
+    structures::string string() const;
     inline operator structures::string() const { return string(); }
+
+    inline bool is_relative() const { return !empty() && _branches[0].size() != 0; }
+    inline bool is_absolute() const { return !is_relative(); };
 
     inline bool empty() const { return _branches.empty(); };
     inline bool is_root() const { return _branches.size() == 1 && _branches[0] == ""; };
 
-    inline structures::string base_name() const { return *_branches.end(); };
+    inline const structures::vector<structures::string>& branches() const { return _branches; };
+    inline size_t amount_of_branches() const { return _branches.size(); }
+
+    inline structures::string base_name() const {
+        return _branches.empty() ? "" : *(_branches.end() - 1);
+    };
     inline structures::string root_name() const {
         return _branches.size() > 0 ? _branches[0] : "";
-    };
-    inline structures::string sub_root_name() const {
-        return _branches.size() > 1 ? _branches[1] : "";
     };
 
     inline structures::string name(size_t i) const { return _branches[i]; }
@@ -36,7 +43,7 @@ class path {
    private:
     structures::vector<structures::string> _branches;
 
-    static structures::vector<structures::string> build_path(structures::string& path_str);
+    static structures::vector<structures::string> build_path(const structures::string& path_str);
 };
 };  // namespace vfs
 };  // namespace influx
--- kernel/vfs/path.cpp
+++ kernel/vfs/path.cpp
@@ -1,29 +1,45 @@
 #include <kernel/vfs/path.h>
 
-influx::vfs::path::path(influx::structures::string &path_str) : _branches(build_path(path_str)) {}
+influx::vfs::path::path(const influx::structures::string &path_str)
+    : _branches(build_path(path_str)) {}
 
 influx::vfs::path &influx::vfs::path::operator=(influx::structures::string path) {
     _branches = build_path(path);
     return *this;
 }
 
-influx::structures::string influx::vfs::path::string() const
-{
-	structures::string path_str;
+bool influx::vfs::path::is_parent(const influx::vfs::path &p) const {
+    // The parent should less branches then this path
+    if (p._branches.size() >= _branches.size()) {
+        return false;
+    }
 
-	// For each branch add it to the path string
-	for (const structures::string &branch : _branches) {
-		path_str += branch + '/';
-	}
+    // For each branch check if it matches the parent's branch
+    for (uint64_t i = 0; i < p._branches.size(); i++) {
+        if (p._branches[i] != _branches[i]) {
+            return false;
+        }
+    }
 
-	// Remove the last divider
-	path_str.resize(path_str.size() - 1);
+    return true;
+}
 
-	return path_str;
+influx::structures::string influx::vfs::path::string() const {
+    structures::string path_str;
+
+    // For each branch add it to the path string
+    for (const structures::string &branch : _branches) {
+        path_str += branch + '/';
+    }
+
+    // Remove the last divider
+    path_str.resize(path_str.size() - 1);
+
+    return path_str;
 }
 
 influx::structures::vector<influx::structures::string> influx::vfs::path::build_path(
-    influx::structures::string &path_str) {
+    const influx::structures::string &path_str) {
     influx::structures::vector<influx::structures::string> branches;
     uint64_t branch_start = 0, branch_size = 0;
 
@@ -31,14 +47,38 @@ influx::structures::vector<influx::structures::string> influx::vfs::path::build_
     for (uint64_t i = 0; i < path_str.size(); i++) {
         // If the character is a divider
         if (path_str[i] == '/') {
-            // Add the current branch
-            branches += structures::string(path_str.data() + branch_start, branch_size);
+            // If this is the first branch or the branch size isn't 0
+            if (branch_start == 0 || branch_size > 0) {
+                // Add the current branch
+                branches += structures::string(path_str.data() + branch_start, branch_size);
+
+                // Check if the relative branch can be trimmed using previous branches
+                if (*(branches.end() - 1) == ".." && branches.size() >= 2) {
+                    branches.pop_back();
+                    branches.pop_back();
+                } else if (*(branches.end() - 1) == "." && branches.size() >= 2) {
+                    branches.pop_back();
+                }
+            }
 
             // Reset branch
             branch_start = i + 1;
             branch_size = 0;
         } else {
             branch_size++;
+        }
+    }
+
+    // Add the last branch
+    if (branch_size != 0) {
+        branches += structures::string(path_str.data() + branch_start, branch_size);
+
+        // Check if the relative branch can be trimmed using previous branches
+        if (*(branches.end() - 1) == ".." && branches.size() >= 2) {
+            branches.pop_back();
+            branches.pop_back();
+        } else if (*(branches.end() - 1) == "." && branches.size() >= 2) {
+            branches.pop_back();
         }
     }
 
