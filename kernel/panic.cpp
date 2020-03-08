1034cf35a78ea22c063c998b4c9ee1b57fb44287
Almog Hamdani <almogh52@gmail.com>
2020-03-08 16:09:28 +0200

    feat: implement more advanced assert and panic functions

--- kernel/panic.cpp
+++ kernel/panic.cpp
@@ -1,9 +1,25 @@
 #include <kernel/panic.h>
 
 #include <kernel/console/console.h>
+#include <kernel/format.h>
+#include <kernel/logger.h>
+#include <stdarg.h>
 
-void kpanic() {
-    influx::console::print("Panicing..\n");
+void kpanic(const char* fmt, ...) {
+    influx::logger log("Kernel Error", influx::console_color::red);
+
+    va_list args;
+    va_start(args, fmt);
+
+    // Verify that the current console isn't null
+    if (influx::console::get_console() != nullptr) {
+        if (fmt) {
+            log.vlog(fmt, args);
+        }
+        log("Panicing..\n");
+    }
+
+    va_end(args);
 
     // Disable interrupts and halt
     __asm__ __volatile__("cli; hlt;");
