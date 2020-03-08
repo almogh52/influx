ccdf1b6326b14a5c04c1b837e0c75f5ad03e6a56
Almog Hamdani <almogh52@gmail.com>
2020-03-08 16:11:56 +0200

    feat: call global constructors after memory init

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,12 +1,15 @@
 #include <kernel/kernel.h>
 
+#include <kernel/assert.h>
 #include <kernel/console/bga_console.h>
 #include <kernel/console/console.h>
 #include <kernel/console/early_console.h>
+#include <kernel/icxxabi.h>
 #include <kernel/logger.h>
 #include <kernel/memory/physical_allocator.h>
 #include <kernel/memory/virtual_allocator.h>
-#include <kernel/icxxabi.h>
+
+extern "C" void _init();
 
 void influx::kernel::start(const boot_info info) {
     early_kmain(info);
@@ -17,6 +20,9 @@ void influx::kernel::early_kmain(const boot_info info) {
     // Init memory manager
     memory::physical_allocator::init(info.memory);
     memory::virtual_allocator::init(info.memory);
+
+    // Init global constructors
+    _init();
 
     // Init early console
     console::set_console(new early_console());
@@ -31,9 +37,11 @@ void influx::kernel::kmain(const boot_info info) {
     log("Finished initializing memory manager and console.\n");
 
     // Init interrupt manager
+    log("Loading interrupt manager..\n");
     _interrupt_manager = new interrupts::interrupt_manager();
 
     // Init driver manager and load drivers
+    log("Loading driver manager..\n");
     _driver_manager = new drivers::driver_manager();
     _driver_manager->load_drivers();
 
@@ -43,10 +51,13 @@ void influx::kernel::kmain(const boot_info info) {
     // Init BGA console
     log("Loading BGA console..\n");
     console::set_console(new bga_console());
+    log("BGA Console loaded.\n");
+
+    kassert(0 == 1);
 
     // Listen to interrupts and halt
     while (true) {
-        __asm__ __volatile__ ("hlt");
+        __asm__ __volatile__("hlt");
     }
 
     // Destruct all global objects
