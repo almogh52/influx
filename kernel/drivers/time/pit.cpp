1986c7c997bd8c30fed55728d128ed125720c6b4
Almog Hamdani <almogh52@gmail.com>
2020-03-11 03:28:57 +0200

    feat: tick time manager on PIT IRQ (#36)

--- kernel/drivers/time/pit.cpp
+++ kernel/drivers/time/pit.cpp
@@ -4,10 +4,14 @@
 #include <kernel/kernel.h>
 #include <kernel/ports.h>
 
-void influx::drivers::pit_irq(influx::interrupts::regs *context,
-                              influx::drivers::pit *pit) {
+void influx::drivers::pit_irq(influx::interrupts::regs *context, influx::drivers::pit *pit) {
     // Increase the counter
     pit->_count++;
+
+    // Tick the time manager
+    if (kernel::time_manager() != nullptr) {
+        kernel::time_manager()->tick();
+    }
 }
 
 influx::drivers::pit::pit() : timer_driver("PIT"), _count(0) {}
@@ -20,7 +24,7 @@ void influx::drivers::pit::load() {
     kernel::interrupt_manager()->set_irq_handler(PIT_IRQ, (uint64_t)influx::drivers::pit_irq, this);
 
     // Set the PIT to square wave generator mode and (lobyte/hibyte) access mode
-	_log("Setting PIT mode and frequency divisor..\n");
+    _log("Setting PIT mode and frequency divisor..\n");
     ports::out<uint8_t>(PIT_MODE, PIT_COMMAND_PORT);
 
     // Send the frequency divisor to the PIT
