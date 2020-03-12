4c5a023ef83356948921eed7f4c5c2e193dc6bf0
Almog Hamdani <almogh52@gmail.com>
2020-03-12 17:57:31 +0200

    feat: allow interrupts lock to be initialized unlocked and lock it afterwards

--- include/kernel/threading/interrupts_lock.h
+++ include/kernel/threading/interrupts_lock.h
@@ -4,8 +4,14 @@ namespace influx {
 namespace threading {
 class interrupts_lock {
    public:
-    interrupts_lock();
+    interrupts_lock(bool lock = true);
     ~interrupts_lock();
+
+    void lock();
+    void unlock();
+
+   private:
+    bool _locked;
 };
 };  // namespace threading
 };  // namespace influx
--- kernel/threading/interrupts_lock.cpp
+++ kernel/threading/interrupts_lock.cpp
@@ -3,12 +3,30 @@
 #include <kernel/interrupts/interrupt_manager.h>
 #include <kernel/kernel.h>
 
-influx::threading::interrupts_lock::interrupts_lock() {
-    // Disable interrupts when object initialized
-    kernel::interrupt_manager()->disable_interrupts();
+influx::threading::interrupts_lock::interrupts_lock(bool lock) : _locked(lock) {
+    // Disable interrupts if the lock need to be locked
+    if (lock) {
+        kernel::interrupt_manager()->disable_interrupts();
+    }
 }
 
 influx::threading::interrupts_lock::~interrupts_lock() {
-    // Re-enable interrupts when object is destroyed
-    kernel::interrupt_manager()->enable_interrupts();
+    // If the lock is locked, re-enable interrupts
+    if (_locked) {
+        kernel::interrupt_manager()->enable_interrupts();
+    }
+}
+
+void influx::threading::interrupts_lock::lock() {
+    // If not locked, disable interrupts
+    if (!_locked) {
+        kernel::interrupt_manager()->disable_interrupts();
+    }
+}
+
+void influx::threading::interrupts_lock::unlock() {
+    // If locked, enable interrupts
+    if (_locked) {
+        kernel::interrupt_manager()->enable_interrupts();
+    }
 }
