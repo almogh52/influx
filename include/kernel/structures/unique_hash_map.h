f7bd1bcaa72889f8f5236f14327004edb540d16d
Almog Hamdani <almogh52@gmail.com>
2020-04-02 22:29:49 +0300

    feat: implement emplace for unique hash map

--- include/kernel/structures/unique_hash_map.h
+++ include/kernel/structures/unique_hash_map.h
@@ -11,9 +11,19 @@ class unique_hash_map : public hash_map<uint64_t, T> {
     inline unique_hash_map(memblock::size_type bucket_count, T empty_item = T())
         : hash_map<uint64_t, T>(bucket_count, empty_item), _counter(0) {}
 
-    inline uint64_t insert_unique(T value) {
-        hash_map<uint64_t, T>::insert(pair<const uint64_t, T>(_counter, value));
+    inline int64_t insert_unique(T value) {
+        if (hash_map<uint64_t, T>::insert(pair<const uint64_t, T>(_counter, value)).second ==
+            false) {
+            return -1;
+        }
+
         return _counter++;
+    }
+
+    template <typename... Args>
+    inline pair<hash_map_iterator<uint64_t, T, hash<uint64_t>>, bool> emplace_unique(
+        Args&&... args) {
+        return hash_map<uint64_t, T>::insert(pair<const uint64_t, T>(_counter, T(args...)));
     }
 
    private:
