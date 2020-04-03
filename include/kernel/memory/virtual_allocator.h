2a59e7110b3dc20461713cec0fad2312b191bd62
Almog Hamdani <almogh52@gmail.com>
2020-04-03 23:32:33 +0300

    fix: free physical pages and allocate new vma nodes correctly

--- include/kernel/memory/virtual_allocator.h
+++ include/kernel/memory/virtual_allocator.h
@@ -7,13 +7,19 @@
 #include <sys/boot_info.h>
 
 #define KERNEL_VMA_START HIGHER_HALF_KERNEL_OFFSET
-#define KERNEL_VMA_SIZE 0x8000000000 // 512GiB - The size of PML4E
+#define KERNEL_VMA_SIZE 0x8000000000  // 512GiB - The size of PML4E
 
 #define VMA_LIST_INITIAL_ADDRESS (HIGHER_HALF_KERNEL_OFFSET + 0x900000)  // 9MiB offset
 
-inline bool operator==(const vma_region_t &a, const vma_region_t &b) { return a.base_addr == b.base_addr; }
-inline bool operator<(const vma_region_t &a, const vma_region_t &b) { return a.base_addr < b.base_addr; }
-inline bool operator>(const vma_region_t &a, const vma_region_t &b) { return a.base_addr > b.base_addr; }
+inline bool operator==(const vma_region_t &a, const vma_region_t &b) {
+    return a.base_addr == b.base_addr;
+}
+inline bool operator<(const vma_region_t &a, const vma_region_t &b) {
+    return a.base_addr < b.base_addr;
+}
+inline bool operator>(const vma_region_t &a, const vma_region_t &b) {
+    return a.base_addr > b.base_addr;
+}
 
 namespace influx {
 namespace memory {
@@ -23,14 +29,18 @@ class virtual_allocator {
    public:
     static void init(const boot_info_mem &mmap);
 
-    static void *allocate(uint64_t size, protection_flags_t pflags, int64_t physical_page_index=-1);
+    static void *allocate(uint64_t size, protection_flags_t pflags,
+                          int64_t physical_page_index = -1);
     static void free(void *ptr, uint64_t size);
 
    private:
     inline static vma_node_t *_vma_list_head = nullptr;
     inline static buffer_t _current_vma_list_page = {.ptr = nullptr, .size = 0};
 
-    static vma_node_t *alloc_vma_node(vma_region_t region);
+    inline static vma_region _vma_list_pending_new_region = {
+        .base_addr = 0, .size = 0, .protection_flags = 0, .allocated = false};
+
+    static vma_node_t *alloc_vma_node(vma_region_t region, bool insert_new_region = true);
 
     static void insert_vma_region(vma_region_t region);
     static bool free_vma_region(vma_region_t region);
@@ -38,9 +48,13 @@ class virtual_allocator {
     static void check_for_vma_node_combination(vma_node_t *node);
 
     static bool address_in_vma_region(vma_region_t &region, uint64_t &address);
-    static vma_region_t find_free_region(uint64_t size, protection_flags_t pflags);
+    static vma_region_t find_free_region(uint64_t size, protection_flags_t pflags,
+                                         vma_region ignore_region = {.base_addr = 0,
+                                                                     .size = 0,
+                                                                     .protection_flags = 0,
+                                                                     .allocated = false});
 
-    static void *allocate(vma_region_t region, int64_t physical_page_index=-1);
+    static void *allocate(vma_region_t region, int64_t physical_page_index = -1);
 };
 };  // namespace memory
 };  // namespace influx
