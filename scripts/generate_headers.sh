fe76cef47f50794f1a983a597f10f9456e4f5a5e
Almog Hamdani <almogh52@gmail.com>
2020-04-06 06:40:27 +0300

    fix: change generated font header to gfx instead of bga

--- scripts/generate_headers.sh
+++ scripts/generate_headers.sh
@@ -9,5 +9,5 @@ CURRNET_DIR=$(pwd)
 # Create header for the font file
 echo -e '\033[0;36mCreating header for font file..\033[0m'
 cd $DIR/../vendor/scalable-font/fonts
-xxd -i u_vga16.sfn > $DIR/../include/kernel/console/bga_u_vga16_font.h
+xxd -i u_vga16.sfn > $DIR/../include/kernel/console/gfx_u_vga16_font.h
 cd $CURRNET_DIR
